8ad22959e1b0316eda49a369f8e132c885b7a540
Gustavo Ian <gustavoian1790@gmail.com>
2022-08-17 11:44:34 -0300

    Fourth commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,32 +4,30 @@ using SP3044149.Models;
 
 namespace SP3044149.Controllers;
 
+public class UserRequest
+{
+    public string? Nome { get; set; }
+    public string? Email { get; set; }    
+}
+
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    public HomeController(ILogger<HomeController> logger) => _logger = logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+    public IActionResult Index() => View();
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    public IActionResult Privacy() => View();
 
-    public IActionResult PrimeiraAction() {
-        return View();
-    }
+    public IActionResult PrimeiraAction() => View();
+
+    public IActionResult Form() => View();
+
+    public string TesteQueryString([FromQuery] string q, [FromQuery] string nome) => $"Chegou aqui {q} e {nome}";
+
+    public string TesteForm([FromForm] UserRequest userRequest) => $"Nome: {userRequest.Nome}, E-mail: {userRequest.Email}";
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
